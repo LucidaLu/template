d1fe8c66317698f483e3fb40e272c765eb6bd081
LucidaLu <luyiren12@yeah.net>
2018-10-07 11:03:34 +0800

    yui

--- Algorithms/MinimumRepresentation.cpp
+++ Algorithms/MinimumRepresentation.cpp
@@ -2,17 +2,17 @@ int MinRep(int b[],int n) {
 	static int a[XN*2];
 	for(int i=1;i<=n;++i)
 		a[i]=a[i+n]=b[i];
-    int p1=1,p2=2;
-    while(p1<=n && p2<=n) {
-        static int len;
-        for(len=0;len<n && a[p1+len]==a[p2+len];++len);
-        if(len==n)
-            break;
+	int p1=1,p2=2;
+	while(p1<=n && p2<=n) {
+		static int len;
+		for(len=0;len<n && a[p1+len]==a[p2+len];++len);
+		if(len==n)
+			break;
 		else
-            a[p1+len]>a[p2+len]?p1=p1+len+1:p2=p2+len+1;
-        if(p1==p2)
-            ++p2;
-    }
-    return std::min(p1,p2);
+			a[p1+len]>a[p2+len]?p1=p1+len+1:p2=p2+len+1;
+		if(p1==p2)
+			++p2;
+	}
+	return std::min(p1,p2);
 }
 

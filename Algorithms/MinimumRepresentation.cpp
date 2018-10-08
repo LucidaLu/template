d7af84bcf99296f74d2da94ae8b6fc7b00706bc2
LucidaLu <luyiren12@yeah.net>
2018-10-08 17:00:58 +0800

    SourceForge

--- Algorithms/MinimumRepresentation.cpp
+++ Algorithms/MinimumRepresentation.cpp
@@ -1,18 +1,15 @@
-int MinRep(int b[],int n) {
-	static int a[XN*2];
-	for(int i=1;i<=n;++i)
-		a[i]=a[i+n]=b[i];
-	int p1=1,p2=2;
-	while(p1<=n && p2<=n) {
-		static int len;
-		for(len=0;len<n && a[p1+len]==a[p2+len];++len);
-		if(len==n)
-			break;
-		else
-			a[p1+len]>a[p2+len]?p1=p1+len+1:p2=p2+len+1;
-		if(p1==p2)
-			++p2;
+int MinimumRepresentation(int *a,int n) {
+	++a;
+	int p1=0,p2=1,len=0;
+	while(p1<n && p2<n && len<n) {
+		if(a[(p1+len)%n]==a[(p2+len)%n])
+			len++;
+		else {
+			(a[(p1+len)%n]>a[(p2+len)%n]?p1:p2)+=len+1;
+			if(p1==p2)
+				p2++;
+			len=0;
+		}
 	}
-	return std::min(p1,p2);
+	return std::min(p1,p2)+1;
 }
-

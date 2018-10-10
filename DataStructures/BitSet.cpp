f4414ca988eed8b226fb9aab98e178d33c297daf
LucidaLu <luyiren12@yeah.net>
2018-10-10 11:18:35 +0800

    shadow

--- DataStructures/BitSet.cpp
+++ DataStructures/BitSet.cpp
@@ -1,16 +1,16 @@
 struct BitSet {
 	unsigned int64 s[(XV>>6)+1];
-    int maxI;
+	int maxI;
 	
 	BitSet(int v):maxI(v>>6) {
-        memset(s,0,sizeof(s));
-    }
-    
+		memset(s,0,sizeof(s));
+	}
+	
 	void Set(unsigned int pos,bool val) {
-        val==0?(s[pos>>6]&=~(1ull<<(pos&63))):(s[pos>>6]|=1ull<<(pos&63));
-    }
+		val==0?(s[pos>>6]&=~(1ull<<(pos&63))):(s[pos>>6]|=1ull<<(pos&63));
+	}
 
-    bool Test(unsigned int pos) const {
-        return s[pos>>6]>>(pos&63)&1;
-    }
+	bool Test(unsigned int pos) const {
+		return s[pos>>6]>>(pos&63)&1;
+	}
 };

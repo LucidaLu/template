463008e3ee2fb67ba6ee09f580954c54d6d22818
LucidaLu <luyiren12@yeah.net>
2018-10-16 20:55:28 +0800

    local

--- NumberTheory/BSGS.cpp
+++ NumberTheory/BSGS.cpp
@@ -4,10 +4,11 @@ int BSGS(int y,int z,int P) {
 		int B=sqrt(P)+0.5;
 		long long zyi=z;
 		for(int i=0;i<=B;i++,(zyi*=y)%=P)
-			if(!S.count(zyi)) S[zyi]=i;
+			if(!S.count(zyi))
+				S[zyi]=i;
 		int yb=Pow(y,B,P);
 		long long ybi=yb;
-		for(int i=1;i<B;i++) {
+		for(int i=1;i<=B;i++) {
 			if(S.count(ybi))
 				return B*i-S[ybi];
 			(ybi*=yb)%=P;

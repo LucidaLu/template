8fbaad2abd0380fc7b4a0c63b8aaba4acd0e54a7
LucidaLu <luyiren12@yeah.net>
2019-02-27 15:24:01 +0800

    Update Basic.cpp

--- Geometry/Basic.cpp
+++ Geometry/Basic.cpp
@@ -11,37 +11,33 @@ double sqr(double const &x) {
 struct Point {
 	double x,y;
 	
-	Point() {}
-	
-	Point(double const &x,double const &y):x(x),y(y) {}
-
 	double Length() const {
 		return sqrt(x*x+y*y);
 	}
 	
 	Point Normal() const {
-		return Point(-y,x);
+		return {-y,x};
 	}
 
 	Point Unit() const {
 		double len=Length();
-		return Point(x/len,y/len);
+		return Point{x/len,y/len};
 	}
 
 	friend Point operator +(const Point &a,const Point &b) {
-		return Point(a.x+b.x,a.y+b.y);
+		return {a.x+b.x,a.y+b.y};
 	}
 
 	friend Point operator -(const Point &a,const Point &b) {
-		return Point(a.x-b.x,a.y-b.y);
+		return {a.x-b.x,a.y-b.y};
 	}
 
 	friend Point operator *(const Point &a,const double &k) {
-		return Point(a.x*k,a.y*k);
+		return Point{a.x*k,a.y*k};
 	}
 
 	friend Point operator /(const Point &a,const double &k) {
-		return Point(a.x/k,a.y/k);
+		return Point{a.x/k,a.y/k};
 	}
 
 	friend double Inner(const Point &a,const Point &b) {
@@ -56,10 +52,6 @@ struct Point {
 struct Line {
 	Point p,v;
 	double ang;
-	Line() {}
-	Line(Point p,Point v):p(p),v(v) {
-		ang=atan2(v.y,v.x);
-	}
 };
 
 double Dist(const Point &a,const Point &b) {
@@ -74,4 +66,3 @@ Point Cross(const Line &ln1,const Line &ln2) {
 	double t=Outer(ln2.v,ln1.p-ln2.p)/Outer(ln1.v,ln2.v);
 	return ln1.p+ln1.v*t;
 }
-

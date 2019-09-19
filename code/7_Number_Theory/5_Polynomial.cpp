8542a7fd668aa86acb50134f11dd5e057c77bb87
LucidaLu <luyiren12@yeah.net>
2019-09-19 10:51:35 +0800

    Update 5_Polynomial.cpp

--- code/7_Number_Theory/5_Polynomial.cpp
+++ code/7_Number_Theory/5_Polynomial.cpp
@@ -1,173 +1,156 @@
-const int XN=1<<18;//Make2(n)*2?
+const int XN=1<<19,//MAKE2(XN)*2
+	 	P=998244353;
 
-namespace Polynomial {
+int inv[XN];
+/*
+inv[1]=1;
+for(int i=2;i<XN;++i)
+	inv[i]=1LL*(P-P/i)*inv[P%i]%P;
+ */
+void M(int &x) {
+	((x>=P) && (x-=P)) || ((x<0) && (x+=P));
+}
 
-	const int P=998244353;
+int Pow(long long base,int v) {
+	long long res;
+	for(res=1;v;v>>=1,(base*=base)%=P)
+		if(v&1)
+			(res*=base)%=P;
+	return res;
+}
 
-	int Add(int x,int const &y) {
-		return (x+=y)>=P?x-P:x;
+int Make2(int x) {
+	return 1<<((32-__builtin_clz(x))+((x&(-x))!=x));
+}
+
+void NTT(int a[],int n,int op) {
+	for(int i=0,j=0;i<n;i++) {
+		if(i>j)
+			std::swap(a[i],a[j]);
+		for(int k=n>>1;(j^=k)<k;k>>=1);
 	}
-
-	int Minus(int x,int const &y) {
-		return (x-=y)<0?x+P:x;
-	}
-
-	int Mul(long long x,int const &y) {
-		return x*y%P;
-	}
-
-	int Pow(long long base,int v) {
-		long long res;
-		for(res=1;v;v>>=1,(base*=base)%=P)
-			if(v&1)
-				(res*=base)%=P;
-		return res;
-	}
-
-	int Inverse(int x,int P=Polynomial::P) {
-		return Pow(x,P-2);
-	}
-
-	int Make2(int x) {
-		return 1<<((32-__builtin_clz(x))+((x&(-x))!=x));
-	}
-
-	void NTT(int a[],int n,int op) {
-		for(int i=1,j=n>>1;i<n-1;++i) {
-			if(i<j)
-				std::swap(a[i],a[j]);
-			int k=n>>1;
-			while(k<=j) {
-				j-=k;
-				k>>=1;
-			}
-			j+=k;
-		}
-		for(int len=2;len<=n;len<<=1) {
-			int rt=Pow(3,(P-1)/len);
-			for(int i=0;i<n;i+=len) {
-				int w=1;
-				for(int j=i;j<i+len/2;++j) {
-					int u=a[j],t=Mul(a[j+len/2],w);
-					a[j]=Add(u,t),a[j+len/2]=Minus(u,t);
-					w=Mul(w,rt);
-				}
+	for(int len=2;len<=n;len<<=1) {
+		int rt=Pow(3,(P-1)/len);
+		for(int i=0;i<n;i+=len) {
+			int w=1;
+			for(int j=i;j<i+len/2;++j) {
+				int u=a[j],t=1LL*a[j+len/2]*w%P;
+				M(a[j]=u+t);M(a[j+len/2]=u-t);
+				w=1LL*w*rt%P;
 			}
 		}
-		if(op==-1) {
-			std::reverse(a+1,a+n);
-			int in=Inverse(n);
-			for(int i=0;i<n;++i)
-				a[i]=Mul(a[i],in);
-		}
 	}
-
-	int Mul(int A[],int An,int B[],int Bn,int R[]) {
-		static int a[XN],b[XN];
-		int n=Make2(An+Bn-1);
-		for(int i=0;i<n;++i) {
-			a[i]=i<An?A[i]:0;
-			b[i]=i<Bn?B[i]:0;
-		}
-		NTT(a,n,1);NTT(b,n,1);
+	if(op==-1) {
+		std::reverse(a+1,a+n);
 		for(int i=0;i<n;++i)
-			a[i]=Mul(a[i],b[i]);
-		NTT(a,n,-1);
-		std::copy(a,a+An+Bn-1,R);
-		return An+Bn-1;
+			a[i]=1LL*a[i]*inv[n]%P;
 	}
+}
 
-	void Inverse(int A[],int An,int R[]) {
-		int n=Make2(An);
-		static int inv[XN],a[XN];
-		inv[0]=Inverse(A[0]);
-		for(int len=2;len<=n;len*=2) {
-			for(int i=0;i<len*2;++i) {
-				inv[i]=i<len/2?inv[i]:0;
-				a[i]=i<std::min(An,len)?A[i]:0;
-			}
-			NTT(inv,len*2,1);NTT(a,len*2,1);
-			for(int i=0;i<len*2;++i)
-				inv[i]=Mul(inv[i],Minus(2,Mul(a[i],inv[i])));
-			NTT(inv,len*2,-1);
+using Polynomial=std::vector<int>;
+
+Polynomial operator *(Polynomial const &A,Polynomial const &B) {
+	static int a[XN],b[XN];
+	int n=Make2((int)A.size()+(int)B.size()-1);
+	for(int i=0;i<n;++i) {
+		a[i]=i<(int)A.size()?A[i]:0;
+		b[i]=i<(int)B.size()?B[i]:0;
+	}
+	NTT(a,n,1);NTT(b,n,1);
+	for(int i=0;i<n;++i)
+		a[i]=1LL*a[i]*b[i]%P;
+	NTT(a,n,-1);
+	return Polynomial(a,a+(int)A.size()+(int)B.size()-1);
+}
+
+Polynomial Inverse(Polynomial const &A){
+	static int a[XN],b[XN];
+	int n=Make2((int)A.size());
+	b[0]=Pow(A[0],P-2);
+	for(int len=2;len<=n;len*=2) {
+		for(int i=0;i<len*2;++i) {
+			a[i]=i<std::min((int)A.size(),len)?A[i]:0;
+			b[i]=i<len/2?b[i]:0;
 		}
-		std::copy(inv,inv+An,R);
+		NTT(a,len*2,1);NTT(b,len*2,1);
+		for(int i=0;i<len*2;++i)
+			M(b[i]=1LL*b[i]*(2-1LL*a[i]*b[i]%P)%P);
+		NTT(b,len*2,-1);
 	}
+	return Polynomial(b,b+(int)A.size());
+}
 
-	void Differentiate(int A[],int n,int R[]) {
-		for(int i=0;i<n-1;++i)
-			R[i]=Mul(A[i+1],i+1);
-		R[n-1]=0;
-	}
+Polynomial Diff(Polynomial const &A) {
+	Polynomial B((int)A.size()-1);
+	for(int i=0;i<(int)A.size()-1;++i)
+		B[i]=1LL*A[i+1]*(i+1)%P;
+	return B;
+}
 
-	void Integrate(int A[],int n,int R[]) {
-		for(int i=n-1;i>=1;--i)
-			R[i]=Mul(A[i-1],Inverse(i));
-		R[0]=0;
-	}
+Polynomial Int(Polynomial const &A) {
+	Polynomial B((int)A.size()+1);
+	for(int i=(int)B.size()-1;i>=1;--i)
+		B[i]=1LL*A[i-1]*inv[i]%P;
+	return B;
+}
 
-	void SquareRoot(int A[],int An,int R[]) {
-		int n=Make2(An);
-		static int irt[XN],rt[XN],a[XN];
-		assert(A[0]==1);rt[0]=1;
-		int i2=Inverse(2);
-		for(int len=2;len<=n;len*=2) {
-			std::fill(rt+len/2,rt+len,0);
-			Inverse(rt,len,irt);
-			for(int i=0;i<len;++i) {
-				a[i]=i<std::min(An,len)?A[i]:0;
-				rt[i]=i<len/2?rt[i]:0;
-				irt[i]=i<len/2?irt[i]:0;
-			}
-			NTT(irt,len,1);NTT(rt,len,1);NTT(a,len,1);
-			for(int i=0;i<len;++i)
-				rt[i]=Mul(i2,Add(Mul(a[i],irt[i]),rt[i]));
-			NTT(rt,len,-1);
+Polynomial SquareRoot(Polynomial const &A) {
+	static int a[XN],b[XN],c[XN];
+	int n=Make2((int)A.size());
+	assert(A[0]==1);b[0]=1;
+	for(int len=2;len<=n;len*=2) {
+		std::fill(b+len/2,b+len,0);
+		auto t=Inverse(Polynomial(b,b+len));
+		for(int i=0;i<len;++i) {
+			a[i]=i<std::min((int)A.size(),len)?A[i]:0;
+			b[i]=i<len/2?b[i]:0;
+			c[i]=i<len/2?t[i]:0;
 		}
-		std::copy(rt,rt+An,R);
+		NTT(c,len,1);NTT(b,len,1);NTT(a,len,1);
+		for(int i=0;i<len;++i)
+			b[i]=1LL*inv[2]*(1LL*a[i]*c[i]%P+b[i])%P;
+		NTT(b,len,-1);
 	}
+	return Polynomial(b,b+(int)A.size());
+}
 
-	void Logarithm(int A[],int An,int R[]) {
-		static int a[XN],b[XN];
-		Differentiate(A,An,b);
-		std::copy(A,A+An,a);
-		Inverse(a,An,a);
-		Mul(a,An,b,An,b);
-		Integrate(b,An,b);
-		std::copy(b,b+An,R);
-	}
+Polynomial Log(Polynomial const &A) {
+	auto B=Int(Diff(A)*Inverse(A));
+	B.resize(A.size());
+	return B;
+}
 
-	void Exponent(int A[],int An,int R[]) {
-		static int a[XN],b[XN],c[XN];
-		int n=Make2(An);
-		b[0]=1;
-		for(int len=2;len<=n;len*=2) {
-			std::fill(b+len/2,b+len,0);
-			Logarithm(b,len,c);
-			for(int i=0;i<len*2;++i) {
-				a[i]=i<std::min(An,len)?A[i]:0;
-				b[i]=i<len/2?b[i]:0;
-				c[i]=i<len?c[i]:0;
-			}
-			NTT(a,len*2,1);NTT(b,len*2,1);NTT(c,len*2,1);
-			for(int i=0;i<len*2;++i)
-				b[i]=Mul(b[i],Add(Minus(1,c[i]),a[i]));
-			NTT(b,len*2,-1);
+Polynomial Exp(Polynomial const &A) {
+	static int a[XN],b[XN],c[XN];
+	int n=Make2((int)A.size());
+	b[0]=1;
+	for(int len=2;len<=n;len*=2) {
+		std::fill(b+len/2,b+len,0);
+		auto logb=Log(Polynomial(b,b+len));
+		for(int i=0;i<len*2;++i) {
+			a[i]=i<std::min((int)A.size(),len)?A[i]:0;
+			b[i]=i<len/2?b[i]:0;
+			c[i]=i<len?logb[i]:0;
 		}
-		std::copy(b,b+An,R);
+		NTT(a,len*2,1);NTT(b,len*2,1);NTT(c,len*2,1);
+		for(int i=0;i<len*2;++i)
+			M(b[i]=1LL*b[i]*(1-c[i]+a[i])%P);
+		NTT(b,len*2,-1);
 	}
+	return Polynomial(b,b+(int)A.size());
+}
 
-	void Pow(int A[],int An,int v,int R[]) {
-		static int a[XN];
-		int A0=A[0],i0=Inverse(A[0]);
-		for(int i=0;i<An;++i)
-			a[i]=Mul(i0,A[i]);
-		Logarithm(a,An,a);
-		for(int i=0;i<An;++i)
-			a[i]=Mul(a[i],v);
-		Exponent(a,An,a);
-		int k=Pow(A0,v);
-		for(int i=0;i<An;++i)
-			R[i]=Mul(a[i],k);
-	}
+Polynomial Pow(Polynomial const &A,int v) {
+	static int a[XN];
+	int A0=A[0],inv0=Pow(A[0],P-2);
+	for(int i=0;i<(int)A.size();++i)
+		a[i]=1LL*A[i]*inv0%P;
+	auto loga=Log(Polynomial(a,a+(int)A.size()));
+	for(int i=0;i<(int)A.size();++i)
+		loga[i]=1LL*loga[i]*v%P;
+	auto exp=Exp(loga);
+	int k=Pow(A0,v);
+	for(int i=0;i<(int)A.size();++i)
+		exp[i]=1LL*exp[i]*k%P;
+	return exp;
 }
